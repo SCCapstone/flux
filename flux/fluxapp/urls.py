2fd6c10a20dc0d995d3a281049bea5d2a82a14d1
cse-brendanmcnichols <mcnichob@email.sc.edu>
2024-11-03 00:04:40 -0400

    deleting django project and creating new

--- flux/fluxapp/urls.py
+++
@@ -1,8 +0,0 @@
-# fluxapp/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('external-api/', views.external_api_view, name='external_api'),
-]
