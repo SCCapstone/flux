3d95e697f63625e5f59acbe78f2759dcc4ac037b
jsykora1 <jakubsykora112@gmail.com>
2024-11-13 15:24:08 -0500

    Add model for books

---
+++ backend/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    authorName = models.CharField(max_length=150)
+    #authorID = models.ForeignKey(User, on_delete.CASCADE, related_name="books")
+    description = models.TextField()
+    isbn10 = models.CharField(max_length=10)
+    isbn13 = models.CharField(max_length=13)
+    genre = models.CharField(max_length=100)
+    coverIMG = models.URLField(max_length=200)
+    publishedDate = models.DateField()
+
+    def __str__(self):
+        return self.title
