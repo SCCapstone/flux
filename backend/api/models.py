78760ea6580aaf7e3991bb8ce4fe81a80d3b7275
Dhruv Patel <89541475+DhruvBPUSC@users.noreply.github.com>
2024-12-04 08:10:42 -0500

    Fixed syntax error
    added semicolon that didnt save into the previous push

--- backend/api/models.py
+++ backend/api/models.py
@@ -45,7 +45,7 @@ class Favorite(models.Model):
     def __str__(self):
         return f"{self.user.username}'s favorite: {self.book.title}"
 
-class Review(models.Model)
+class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     added_date = models.DateTimeField(auto_now_add=True)
